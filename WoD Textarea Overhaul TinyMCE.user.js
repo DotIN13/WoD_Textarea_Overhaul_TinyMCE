442d4d757e82e677f94856538e0a614dc28fb490
DotIN13 <tiger.owe.me@gmail.com>
2020-02-10 15:19:44 +0800

    Update WoD Textarea Overhaul TinyMCE.user.js

--- WoD Textarea Overhaul TinyMCE.user.js
+++ WoD Textarea Overhaul TinyMCE.user.js
@@ -1,38 +1,43 @@
 // ==UserScript==
 // @name         WoD Textarea Overhaul TinyMCE
 // @namespace    github.com/DotIN13
-// @version      0.1
+// @version      0.3
+// @description  Refreshing WoD textareas with TinyMCE v5
 // @require      https://cdn.tiny.cloud/1/ywt4e8tywe5b6elzc8f93q7e89loo38uw7l9335movroj7be/tinymce/5/tinymce.min.js
 // @require      https://github.com/DotIN13/WoD_Textarea_Overhaul_TinyMCE/raw/master/zh_CN.js
 // @updateURL    https://github.com/DotIN13/WoD_Textarea_Overhaul_TinyMCE/raw/master/WoD%20Textarea%20Overhaul%20TinyMCE.user.js
-// @description  Refreshing Wod textareas with TinyMCE
 // @author       DotIN13
-// @match        http://canto.world-of-dungeons.org/*
+// @match        http://canto.world-of-dungeons.org/wod/spiel/hero/profile.php?menukey=hero_profiles&session_hero_id=*
+// @match        http://canto.world-of-dungeons.org/wod/spiel/forum/*
+// @match        http://canto.world-of-dungeons.org/wod/spiel/dungeon/group.php*
+// @match        http://canto.world-of-dungeons.org/wod/spiel/pm*
 // @grant        none
 // ==/UserScript==
 
-(function()
-{
+(function() {
     'use strict';
+
+    //romove annoying WoD original resizer
+    new MutationObserver(function(mutations) {
+    if (document.getElementsByTagName('textarea')[0]) {
+        var seltextarea = document.getElementsByTagName('textarea')[0];
+        if (seltextarea) {
+            seltextarea.removeAttribute("onmouseover");
+            this.disconnect();// disconnect the observer
+        }
+    }
+    }).observe(document, {childList: true, subtree: true});
 
     //prevent the left collumn from covering the source code dialog
     document.getElementsByClassName("gadget_fixed_container")[0].setAttribute("style", "position: fixed; left: 0px; top: 0px; width: 160px; z-index: 1;");
 
-    //romove annoying WoD original resizer
-    var textareas = document.querySelectorAll("textarea");
-    console.log(textareas);
-    for (var i = 0; i < textareas.length; i++)
-    {
-        textareas[i].removeAttribute("onmouseover");
-    }
-    console.log("resizer removed.")
-
-    //init tinyMCE
     tinymce.init(
     {
         selector: 'textarea',
         language: "zh_CN",
-        plugins: "bbcode code",
+        plugins: "bbcode code autoresize",
+        min_height:300,
+        max_height: 800,
         toolbar: 'undo redo | bold italic underline | code',
     });
 })();
